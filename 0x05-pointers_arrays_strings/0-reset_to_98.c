49884a85e3ff7b83691c827342bad27adc3908da
mariammagedfa <mariammagedfa@yahoo.com>
2023-03-17 05:41:39 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Change the value from integer to 98.
+ * @n: Pointer to an int
+ *
+ *Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - Swap two integers
+ * @a: first value
+ * @b: second value
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
