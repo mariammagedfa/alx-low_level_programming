d54f62c2460662f7b1f537dc320f15be65ff7354
mariammagedfa <mariammagedfa@yahoo.com>
2023-03-17 05:24:23 -0700

    0x04

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ * @n: number to return
+ * Return: void
+ */
+
+void print_number(int n)
+{
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (!(n / 10))
+		_putchar('0' + n % 10);
+	else
+	{
+		print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Check digit
+ * @a: First number
+ * @b: second number
+ *
+ * Return: Multiplication of two numbers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
