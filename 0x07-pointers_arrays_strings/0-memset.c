048c91ba8482a6a727d612518f90f8c9f4fdeebc
mariammagedfa <mariammagedfa@yahoo.com>
2023-03-17 07:15:44 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main"
+/**
+ * _memset - fill memory with a constant byte.
+ * @s:Pointer to the block of memory to fill.
+ * @b:Value to be set. The value is passed as an int.
+ * @n:Number of bytes to be set to the value.
+ *
+ *Return: Pointer to block memory filled.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count = 0;
+	char *p = s;
+
+	while (count < n)
+	{
+		*p = b;
+		count++;
+		p++;
+	}
+	return (s);
+}
