8948b12163405a67288491c55ad67063e5895c76
mariammagedfa <mariammagedfa@yahoo.com>
2023-03-21 14:28:20 -0700

    0x02

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ *main - print first 98 fibonacci
  *
- * Return: Always 0
+ *Return: 0 always.
  */
-
 int main(void)
 {
-	float first, second, next;
-	int c;
+	unsigned long int a = 1, b = 2, c, k, p, l, o, m, u, d, i;
 
-	first = 1;
-	second = 2;
-	c = 2;
+	c = a + b;
+	printf("%lu, %lu, ", a, b);
+	d = 3;
 
-	printf("%.f, %.f", first, second);
-	while (c  < 98)
+	while (d < 89)
 	{
-		next = first + second;
-		first = second;
-		second = next;
-		printf(", %.f", next);
-		c++;
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		c = a + b;
+		d++;
 	}
-	printf("\n");
+	l = b / 1000000000;
+	o = b % 1000000000;
+	m = c / 1000000000;
+	u = c % 1000000000;
+	for (i = 89; i < 98; i++)
+	{
+		printf("%lu%lu, ", m, u);
+		k = l;
+		p = o;
+		l = m;
+		o = u;
+		m = k + l + ((p + o) / 1000000000);
+		u = (p + o) % 1000000000;
+	}
+	printf("%lu%lu\n", m, u);
 	return (0);
 }
