960962fb2ae069028658f0ac03dee254686d0954
mariammagedfa <mariammagedfa@yahoo.com>
2023-03-21 13:24:30 -0700

    0x02

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,28 @@
-#include "main.h"
-/**
- * print_sign - a function that prints the sign of a number
- * @n: number input
- * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
- */
-int print_sign(int c)
-{
-	char sign;
-	int value;
+#include "holberton.h"
 
-	if (c > 0)
+/**
+ * print_sign - Write a function that prints the sign of a number.
+ *
+ * @n: number to be check
+ *
+ * Return: Return symbols if n is greater than or less than
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
 	{
-		value = 1;
-		sign = '+';
+		_putchar('0');
+		return (0);
 	}
-	else if (c < 0)
+	else if (n > 0)
 	{
-		value = -1;
-		sign = '-';
+		_putchar('+');
+		return (1);
 	}
 	else
 	{
-		value = 0;
-		sign = '0';
+		_putchar('-');
+		return (-1);
 	}
-	_putchar(sign);
-	return (value);
 }
