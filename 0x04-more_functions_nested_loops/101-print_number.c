20e43fdc9ddb91fd533f8bc4d5021586f96f0ccc
mariammagedfa <mariammagedfa@yahoo.com>
2023-03-23 09:18:38 -0700

    0x04

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,21 @@
 #include "main.h"
-
 /**
- * print_number - print number
- * @n: number to return
- * Return: void
+ *print_number - print integer.
+ *@n: integer.
+ *
+ *Return: void.
  */
-
 void print_number(int n)
 {
+	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
-		n = -n;
-		_putchar('-');
+		_putchar(45);
+		x = -n;
 	}
-	if (!(n / 10))
-		_putchar('0' + n % 10);
-	else
-	{
-		print_number(n / 10);
-		_putchar('0' + n % 10);
-	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
