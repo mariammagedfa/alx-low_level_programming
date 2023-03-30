38b38ea9184bc1939f94842d0caa1ae8e34d3e28
mariammagedfa <mariammagedfa@yahoo.com>
2023-03-29 18:32:02 -0700

    0x06

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - print specific number
+ * @n - location
+ * *@p - pointer to place
+ * @a - arrray
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n;
