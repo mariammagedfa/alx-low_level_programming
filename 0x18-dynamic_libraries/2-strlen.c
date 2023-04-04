c8675dbcf26c84a3623ddfb0a006aa06b12a8d4c
mariammagedfa <mariammagedfa@yahoo.com>
2023-04-04 07:05:15 -0700

    0x18

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - Calculate the lenght of a string
+ * @s: string
+ *
+ * Return: lenght of the s string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		c++;
+	}
+	return (c);
+}
