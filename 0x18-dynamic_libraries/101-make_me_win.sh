e363d8ec77601b513116a13d1bd0d5b1e6a9b937
mariammagedfa <mariammagedfa@yahoo.com>
2023-06-23 14:28:48 -0700

    00

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/mariammagedfa/alx-low_level_programming/main/0x18-dynamic_libraries/dl
-export LD_PRELOAD=/tmp/ld.so
+wget -P /tmp/ https://raw.github.com/mariammagedfa/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
