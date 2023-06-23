2f757b4f7e1c5ec5527bc37e8dd7f83f3e96d33f
mariammagedfa <mariammagedfa@yahoo.com>
2023-06-23 13:37:39 -0700

    00

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/bash
-gcc -Wall -c -fPIC *.c
-gcc  -shared *.o  -o liball.so
+#!/bin/bash
+gcc *.c -c -fPIC
+gcc *.o -shared -o liball.so
+export LD_LIBRARY_PATH=:/home/vagrant/alx-low_level_programming/0x18-dynamic_libraries
